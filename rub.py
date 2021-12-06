54a9e9cc2c82f68db499ed1891d4416ad247e892
Irishusha <95314489+Irishusha@users.noreply.github.com>
2021-12-06 22:50:13 +0200

    Add files via upload

--- rub.py
+++ rub.py
@@ -1,10 +1,13 @@
+# put your python code here
 k=int(input())
 if k < 0 or k > 99:
-    print("error")
+  print("ошибка")
+elif k in [11,12,13,14]:
+    print(k, "рублей")
+elif k % 10 == 1:
+  print(k, "рубль")
 else :
-    if k == 1:
-        print(k, " рубль")
-    if k % 10 in [2,3,4]:
-        print(k, " рубля")
-    else :
-        print(k, " рублей")
+  if k % 10 in [2,3,4]:
+    print(k, "рубля")
+  else :
+    print(k, "рублей")
