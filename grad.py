bc43e22515fd7fd9dad2b7f1c5df43a6be5595a7
Iryna <95314489+Irishusha@users.noreply.github.com>
2021-12-08 11:37:32 +0200

    Add files via upload

---
+++ grad.py
@@ -0,0 +1,10 @@
+#расчитывает значение угла в градусах между стрелкой часов 12:00 и введенными данными
+h=int(input())
+m=int(input())
+s=int(input())
+if h<=0 or h>=12 or m<0 or m>=60 or s<0 or s>=60:
+    print ("error")
+else :
+    grad=round(h*30+m*0.5+s*0.0083333333,2)
+    print(grad)
+
