a5f453a5339097898d5095fe5dcd4631cce25a67
Iryna <95314489+Irishusha@users.noreply.github.com>
2021-12-08 16:47:31 +0200

    Add files via upload

---
+++ son.py
@@ -0,0 +1,12 @@
+A=int(input())
+B=int(input())
+H=int(input())
+if B < A:
+    print("error")
+else :    
+        if H < A :
+            print("Недосып")
+        elif H > B:
+            print("Пересып")
+        else :
+            print("Это нормально")
