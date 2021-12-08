0d82174be292cdbd51767d273a05cb62c3ce89e0
Iryna <95314489+Irishusha@users.noreply.github.com>
2021-12-08 11:45:52 +0200

    Add files via upload

--- grad.py
+++ grad.py
@@ -2,9 +2,9 @@
 h=int(input())
 m=int(input())
 s=int(input())
-if h<=0 or h>=12 or m<0 or m>=60 or s<0 or s>=60:
+if h<0 or h>=12 or m<0 or m>=60 or s<0 or s>=60:
     print ("error")
 else :
-    grad=round(h*30+m*0.5+s*0.0083333333,2)
+    grad=round(h*30+m*0.5+s*(0.5/60),2)
     print(grad)
 
