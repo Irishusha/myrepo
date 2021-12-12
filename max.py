0b1548b7c5d5d7c8a539cef43a34593b94e6d59e
Irishusha <irishushap@gmail.com>
2021-12-12 18:23:11 +0200

    12

--- max.py
+++ max.py
@@ -4,15 +4,12 @@ c=int(input())
 max, min, mid = a,a,a
 if b >= max:
     max = b
-  #  mid = a
 if c >= max :
     max = c
-   # mid = a   
 if b <= min :
     min = b
 if c <= min :
     min = c
-    #mid = b
 z=a+b+c
 mid=z-max-min
 print(max)
